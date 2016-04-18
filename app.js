09481250816e2432c4c0790b9bd43be84632330b
Celso Rodrigues <celso.cunha.rodrigues@gmail.com>
2016-04-18 23:59:28 +0100

    bug fix--heroku app error.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app=express();
 app.set('port', process.env.PORT||process.argv[2]||3000);
 app.use(express.static(__dirname+'/public'));
 
+app.get('/about',function(req, res){
+	res.send('about');
+
+});
 
 app.listen(app.get('port'), function(){
 	console.log('Server started at localhost:'+app.get('port')+'. Please press ctrl+c to kill it');
