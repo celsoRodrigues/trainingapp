cc517a25308e78c62c9da896d0af1908bff289c8
Celso Rodrigues <celso.cunha.rodrigues@gmail.com>
2016-04-18 23:34:40 +0100

    basic working server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var app=express();
+app.set('port', process.env.PORT||process.argv[2]||3000);
+app.use(express.static(__dirname+'/public'));
+
+
+app.listen(app.get('port'), function(){
+	console.log('Server started at localhost:'+app.get('port')+'. Please press ctrl+c to kill it');
+
+});
